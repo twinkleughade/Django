63f3f881c68c782fd3a0b91527e8333f05679887
twinkleughade <twinklethakre1998@gmail.com>
2025-01-14 23:43:17 +0530

    static

---
+++ static_project/project/app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+def pqr(request):
+    return render (request,'navbar.html')
+
+def home(request):
+    return render (request,'home.html')
+
+def about(request):
+    return render (request,'about.html')
+
+def contact(request):
+    return render (request,'contact.html')
+
+def register(request):
+    return render (request,'register.html')
+
+def login(request):
+    return render (request, 'login.html')
