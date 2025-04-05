6ad0a9637e8e998a970c42ae3278b304860dcae0
twinkleughade <twinklethakre1998@gmail.com>
2025-04-05 13:53:55 +0530

    template cutting

--- render_response/project/app/views.py
+++ render_response/project/app/views.py
@@ -4,7 +4,15 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request):
     # return redirect('/index/')
-    return redirect('/index/')
+    name,city='twinkle','bhopal'
+    # return redirect(f'/index/?name={name}&city={city}')
+    return redirect('/index/?name={}&city={}.format(name,city)')
 
 def index(request):
-    return HttpResponse("hello through redirect")
+    print(request.method)
+    print(request.GET)
+    x=request.GET.get('name')
+    y=request.GET.get('city')
+    print(x,y)
+
+    # return HttpResponse("hello through redirect")
